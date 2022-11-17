8faaac6ab50dc9069890797c64ef84264f3b2ac0
Alhaji Sesay <alhaji8765@gmail.com>
2022-11-16 20:39:24 -0800

    file should contain your main function only.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,8 +11,7 @@ int main(int argc, char *argv[])
 	int arg1, arg2, result;
 	char o;
 
-	
-	int(*func)(int, int);
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -25,7 +24,7 @@ int main(int argc, char *argv[])
 
 	func =  get_op_func(argv[2]);
 
-	if(!func)
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
@@ -33,7 +32,7 @@ int main(int argc, char *argv[])
 
 	o = *argv[2];
 
-	if ((o == '/' || o =='%') && arg2 == 0)
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
